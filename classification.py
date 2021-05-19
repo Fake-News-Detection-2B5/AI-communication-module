74b7c739c98a66516e9b97dd6e0db322aeb498ad
mosorandrei <75736520+mosorandrei@users.noreply.github.com>
2021-05-19 14:02:32 +0300

    Added Bert Module

--- classification.py
+++ classification.py
@@ -22,8 +22,13 @@ from sklearn.ensemble import GradientBoostingClassifier
 from tensorflow.keras.preprocessing.text import Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification, AdamW, BertConfig, \
+    get_linear_schedule_with_warmup
+from torch.utils.data import TensorDataset, random_split, DataLoader, RandomSampler, SequentialSampler
 
 warnings.filterwarnings("ignore")
+
 
 class Model:
     def __init__(self, text):
@@ -38,11 +43,12 @@ class Model:
     def predict(self):
         pass
 
+
 class BiLstm(Model):
     def __init__(self, text):
         super().__init__(text)
         self.model = tf.keras.models.load_model('models/Bi-Lstm/model')
-        with open('models/Bi-Lstm/tokenizer.pickle', 'rb') as f:
+        with open('../../Desktop/AI-communication-module/models/Bi-Lstm/tokenizer.pickle', 'rb') as f:
             self.tokenizer = pickle.load(f)
 
     def download_dependencies(self):
@@ -92,7 +98,7 @@ class Sentiment(Model):
     def __init__(self, text):
         super().__init__(text)
         self.model = tf.keras.models.load_model('models/SentimentAnalysis/model')
-        with open('models/SentimentAnalysis/tokenizer.pickle', 'rb') as f:
+        with open('../../Desktop/AI-communication-module/models/SentimentAnalysis/tokenizer.pickle', 'rb') as f:
             self.tokenizer = pickle.load(f)
         self.TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
 
@@ -125,105 +131,110 @@ class Sentiment(Model):
             elif prediction[0] == 3:
                 return "other"
 
+
 class Three_Layer(Model):
-	def __init__(self, text):
-		super().__init__(text)
+    def __init__(self, text):
+        super().__init__(text)
 
-	def download_dependencies(self):
-		try:
-			nltk.data.find('stopwords')
-			nltk.data.find('wordnet')
-			nltk.data.find('words')
-		except LookupError:
-			nltk.download('stopwords')
-			nltk.download('wordnet')
-			nltk.download('words')
+    def download_dependencies(self):
+        try:
+            nltk.data.find('stopwords')
+            nltk.data.find('wordnet')
+            nltk.data.find('words')
+        except LookupError:
+            nltk.download('stopwords')
+            nltk.download('wordnet')
+            nltk.download('words')
 
-	def process_text(self):
-		self.clean_text = clean_text(self.text)
-       
-		nlp = spacy.load('en_core_web_sm')
-		pos_tags_column = []
+    def process_text(self):
+        self.clean_text = clean_text(self.text)
 
-		for text in self.text.split():
-			pos_tags = []
-			doc = nlp(text)
-			for token in doc:
-				pos_tags.append(token.pos_)
-			all_pos_tags = ' '.join(pos_tags)
-			pos_tags_column.append(all_pos_tags)
-    
-		self.POS_text = pos_tags_column
+        nlp = spacy.load('en_core_web_sm')
+        pos_tags_column = []
 
-		lexicon = Empath()
-		semantic = []
-		count = 0
+        for text in self.text.split():
+            pos_tags = []
+            doc = nlp(text)
+            for token in doc:
+                pos_tags.append(token.pos_)
+            all_pos_tags = ' '.join(pos_tags)
+            pos_tags_column.append(all_pos_tags)
 
-		d = lexicon.analyze(self.text.split(), normalize=False)
-		x = []
-		for key, value in d.items():
-			x.append(value)
-		x = np.asarray(x)
-		semantic.append(x)
+        self.POS_text = pos_tags_column
 
-		self.semantic_text = semantic
+        lexicon = Empath()
+        semantic = []
+        count = 0
 
-		categories = []
-		a = lexicon.analyze("")
-		for key, value in a.items():
-			categories.append(key)
+        d = lexicon.analyze(self.text.split(), normalize=False)
+        x = []
+        for key, value in d.items():
+            x.append(value)
+        x = np.asarray(x)
+        semantic.append(x)
 
-		sem = []
-		a = []
-		for j in range(len(semantic[0])):
-			for k in range(int(semantic[0][j])):
-				a.append(categories[j])
-		b = " ".join(a)
-		sem.append(b)
+        self.semantic_text = semantic
 
-		self.semantics_text = sem
+        categories = []
+        a = lexicon.analyze("")
+        for key, value in a.items():
+            categories.append(key)
 
-		X_test_text = self.clean_text
-		X_test_POS = self.POS_text
-		X_test_sem = self.semantics_text
+        sem = []
+        a = []
+        for j in range(len(semantic[0])):
+            for k in range(int(semantic[0][j])):
+                a.append(categories[j])
+        b = " ".join(a)
+        sem.append(b)
 
-		empty = ""
-		for pos in X_test_POS:
-			empty += pos + " "
-		X_test_POS = [empty]
+        self.semantics_text = sem
 
+        X_test_text = self.clean_text
+        X_test_POS = self.POS_text
+        X_test_sem = self.semantics_text
 
-		loaded_tfidf = pickle.load(open('./models/Three_Layer/vectorizers/tfidf_pickle.sav', 'rb'))
-		loaded_pos = pickle.load(open('./models/Three_Layer/vectorizers/pos_pickle.sav', 'rb'))
-		loaded_sem = pickle.load(open('./models/Three_Layer/vectorizers/sem_pickle.sav', 'rb'))
-       
-		tfidf_test = loaded_tfidf.transform([X_test_text])
-		pos_tfidf_test = loaded_pos.transform(X_test_POS)
-		sem_tfidf_test = loaded_sem.transform(X_test_sem)
+        empty = ""
+        for pos in X_test_POS:
+            empty += pos + " "
+        X_test_POS = [empty]
 
-		text_w = 0.5 * 3
-		pos_w = 0.15 * 3
-		sem_w = 0.35 * 3
+        loaded_tfidf = pickle.load(open(
+            '../../Desktop/AI-communication-module/models/Three_Layer/vectorizers/tfidf_pickle.sav', 'rb'))
+        loaded_pos = pickle.load(open(
+            '../../Desktop/AI-communication-module/models/Three_Layer/vectorizers/pos_pickle.sav', 'rb'))
+        loaded_sem = pickle.load(open(
+            '../../Desktop/AI-communication-module/models/Three_Layer/vectorizers/sem_pickle.sav', 'rb'))
 
-		tfidf_test *= text_w
-		pos_tfidf_test *= pos_w
-		sem_tfidf_test *= sem_w
+        tfidf_test = loaded_tfidf.transform([X_test_text])
+        pos_tfidf_test = loaded_pos.transform(X_test_POS)
+        sem_tfidf_test = loaded_sem.transform(X_test_sem)
 
-		diff_n_rows = pos_tfidf_test.shape[0] - tfidf_test.shape[0]
-		d = sp.vstack((tfidf_test, sp.csr_matrix((diff_n_rows, tfidf_test.shape[1]))))
-		e = sp.hstack((pos_tfidf_test, d))
+        text_w = 0.5 * 3
+        pos_w = 0.15 * 3
+        sem_w = 0.35 * 3
 
-		diff_n_rows = e.shape[0] - sem_tfidf_test.shape[0]
-		d = sp.vstack((sem_tfidf_test, sp.csr_matrix((diff_n_rows, sem_tfidf_test.shape[1]))))
+        tfidf_test *= text_w
+        pos_tfidf_test *= pos_w
+        sem_tfidf_test *= sem_w
 
-		self.X_test = sp.hstack((e, d))
+        diff_n_rows = pos_tfidf_test.shape[0] - tfidf_test.shape[0]
+        d = sp.vstack((tfidf_test, sp.csr_matrix((diff_n_rows, tfidf_test.shape[1]))))
+        e = sp.hstack((pos_tfidf_test, d))
 
-	def load_model(self):
-		self.loaded_model = pickle.load(open('./models/Three_Layer/three_layer_pickle.sav', 'rb'))
+        diff_n_rows = e.shape[0] - sem_tfidf_test.shape[0]
+        d = sp.vstack((sem_tfidf_test, sp.csr_matrix((diff_n_rows, sem_tfidf_test.shape[1]))))
 
-	def predict(self):
-		self.load_model()
-		return self.loaded_model.predict(self.X_test)[0].lower()
+        self.X_test = sp.hstack((e, d))
+
+    def load_model(self):
+        self.loaded_model = pickle.load(open(
+            '../../Desktop/AI-communication-module/models/Three_Layer/three_layer_pickle.sav', 'rb'))
+
+    def predict(self):
+        self.load_model()
+        return self.loaded_model.predict(self.X_test)[0].lower()
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Fake News Classification')
@@ -231,23 +242,163 @@ def parse_args():
     args = parser.parse_args()
     return ' '.join(args.text)
 
-def clean_text(text):
-	text = re.sub('['+string.punctuation+']','', text)
-	text = re.sub(r"[-()\"#/@’;:<>{}`+=~|.!?,]", '', text)
-	text = text.lower().split()
 
-	stops = set(stopwords.words("english"))
-	text = [w for w in text if w not in stops]
-	text = " ".join(text)
-  
-	text = re.sub(r'[^a-zA-Z\s]', u'', text, flags=re.UNICODE)
-  
-	text = text.split()
-	l = WordNetLemmatizer()
-	lemmatized_words = [l.lemmatize(word) for word in text if len(word) > 2]
-	text = " ".join(lemmatized_words)
-    
-	return text
+def clean_text(text):
+    text = re.sub('[' + string.punctuation + ']', '', text)
+    text = re.sub(r"[-()\"#/@’;:<>{}`+=~|.!?,]", '', text)
+    text = text.lower().split()
+
+    stops = set(stopwords.words("english"))
+    text = [w for w in text if w not in stops]
+    text = " ".join(text)
+
+    text = re.sub(r'[^a-zA-Z\s]', u'', text, flags=re.UNICODE)
+
+    text = text.split()
+    l = WordNetLemmatizer()
+    lemmatized_words = [l.lemmatize(word) for word in text if len(word) > 2]
+    text = " ".join(lemmatized_words)
+
+    return text
+
+
+class BertModel(Model):
+    def __init__(self, model, text):
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
+        self.model = model
+        self.text = self.preprocess(text)
+        self.tokenizer = BertTokenizer.from_pretrained('bert-large-uncased', do_lower_case=True)
+
+    def tokenize_map(self, sentence, labs='None'):
+        global labels
+        input_ids = []
+        attention_masks = []
+
+        for text in sentence:
+            #   (1) Tokenize the sentence.
+            #   (2) Prepend the `[CLS]` token to the start.
+            #   (3) Append the `[SEP]` token to the end.
+            #   (4) Map tokens to their IDs.
+            #   (5) Pad or truncate the sentence to `max_length`
+            #   (6) Create attention masks for [PAD] tokens.
+
+            encoded_dict = self.tokenizer.encode_plus(
+                text,
+                add_special_tokens=True,  # Add '[CLS]' and '[SEP]'
+                truncation='longest_first',  # Activate and control truncation
+                max_length=72,  # Max length according to our current RAM constraints.
+                pad_to_max_length=True,  # Pad & truncate all sentences.
+                return_attention_mask=True,  # Construct attn. masks.
+                return_tensors='pt',  # Return pytorch tensors.
+            )
+
+            # Add the encoded sentence to the id list.
+            input_ids.append(encoded_dict['input_ids'])
+
+            # And its attention mask (differentiates padding from non-padding).
+            attention_masks.append(encoded_dict['attention_mask'])
+
+        # Convert the lists into tensors.
+        input_ids = torch.cat(input_ids, dim=0)
+        attention_masks = torch.cat(attention_masks, dim=0)
+
+        if labs != 'None':  # Setting this for using this definition for both train and test data so labels won't be a problem in our outputs.
+            labels = torch.tensor(labels)
+            return input_ids, attention_masks, labels
+        else:
+            return input_ids, attention_masks
+
+    def floatToString(self, text):
+        try:
+            float(text)
+            return str(text)
+        except ValueError:
+            return text
+
+    def line_removal(self, text):
+        all_list = [char for char in text if char not in "…-–—_©“”‘’•⋆»"]
+        clean_str = ''.join(all_list)
+        return clean_str
+
+    def punctuation_removal(self, text):
+        all_list = [char for char in text if char not in string.punctuation]
+        clean_str = ''.join(all_list)
+        return clean_str
+
+    def preprocess(self, text, stem=False):
+        stop_words = stopwords.words('english')
+        stemmer = SnowballStemmer('english')
+        lemmatizer = WordNetLemmatizer()
+        text = self.floatToString(text)
+        text = text.lower()
+        text = self.line_removal(text)
+        text = self.punctuation_removal(text)
+        text = re.sub(r'[!]+', '!', text)
+        text = re.sub(r'[?]+', '?', text)
+        text = re.sub(r'[.]+', '.', text)
+        text = re.sub(r"'", "", text)
+        text = re.sub('\s+', ' ', text).strip()
+        text = re.sub(r'&amp;?', r'and', text)
+        text = re.sub(r"https?:\/\/t.co\/[A-Za-z0-9]+", "", text)
+        text = re.sub(r'[:"$%&\*+,-/:;<=>@\\^_`{|}~]+', '', text)
+        emoji_pattern = re.compile("["
+                                   u"\U0001F600-\U0001F64F"  # Emoticons
+                                   u"\U0001F300-\U0001F5FF"  # Symbols & Pictographs
+                                   u"\U0001F680-\U0001F6FF"  # Transport & Map Symbols
+                                   u"\U0001F1E0-\U0001F1FF"  # Flags (iOS)
+                                   u"\U00002702-\U000027B0"
+                                   u"\U000024C2-\U0001F251"
+                                   "]+", flags=re.UNICODE)
+        text = emoji_pattern.sub(r'EMOJI', text)
+
+        tokens = []
+        for token in text.split():
+            if token not in stop_words:
+                tokens.append(lemmatizer.lemmatize(token))
+        return " ".join(tokens)
+
+    def predict(self):
+        test_input_ids_f, test_attention_masks_f = self.tokenize_map(self.text)
+        prediction_data_f = TensorDataset(test_input_ids_f, test_attention_masks_f)
+        prediction_sampler_f = SequentialSampler(prediction_data_f)
+        prediction_dataloader_f = DataLoader(prediction_data_f, sampler=prediction_sampler_f, batch_size=12)
+        self.model.eval()
+        predictions = []
+
+        for batch in prediction_dataloader_f:
+            batch = tuple(t.to(self.device) for t in batch)
+            b_input_ids, b_input_mask, = batch
+
+            with torch.no_grad():
+                outputs = self.model(b_input_ids, token_type_ids=None,
+                                     attention_mask=b_input_mask)
+            logits = outputs[0]
+            logits = logits.detach().cpu().numpy()
+            predictions.append(logits)
+
+        print('    DONE.')
+
+        flat_predictions = [item for sublist in predictions for item in sublist]
+        flat_predictions = np.argmax(flat_predictions, axis=1).flatten()
+
+        preds = pd.DataFrame()
+        preds["our rating"] = flat_predictions
+
+        pred1 = []
+        for i in range(len(preds)):
+            if preds['our rating'][i] == 0:
+                preds['our rating'][i] = 'false'
+            if preds['our rating'][i] == 3:
+                preds['our rating'][i] = 'true'
+            if preds['our rating'][i] == 1:
+                preds['our rating'][i] = 'other'
+            if preds['our rating'][i] == 2:
+                preds['our rating'][i] = 'partially false'
+
+        return preds['our rating'][0]
 
 
 if __name__ == '__main__':
@@ -271,8 +422,18 @@ if __name__ == '__main__':
     three_layer.process_text()
     prediction_three_layer = three_layer.predict()
 
-    c = Counter([prediction_bilstm, prediction_sentiment, prediction_three_layer])
+    # Bert
+    try:
+        loaded_model = pickle.load(open('models/Bert/finalized_model.sav', 'rb'))
+
+        bert = BertModel(loaded_model, text)
+        prediction_bert = bert.predict()
+        print(prediction_bert)
+    except:
+        prediction_bert = 'other'
+
+    c = Counter([prediction_bilstm, prediction_sentiment, prediction_bert, prediction_three_layer])
     value, count = c.most_common()[0]
     if value:
-        with open ('scor.txt', 'w') as f:
+        with open('scor.txt', 'w') as f:
             f.write(value)
