6282e5ad2e4db7277f99e0d1c74e9eec4364100f
adipeterca <adipeterca@gmail.com>
2021-05-24 22:09:50 +0300

    :)

--- classification.py
+++ classification.py
@@ -94,42 +94,42 @@ class BiLstm(Model):
                 return "other"
 
 
-class Sentiment(Model):
-    def __init__(self, text):
-        super().__init__(text)
-        self.model = tf.keras.models.load_model('models/SentimentAnalysis/model')
-        with open('models/SentimentAnalysis/tokenizer.pickle', 'rb') as f:
-            self.tokenizer = pickle.load(f)
-        self.TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+# class Sentiment(Model):
+#     def __init__(self, text):
+#         super().__init__(text)
+#         self.model = tf.keras.models.load_model('models/SentimentAnalysis/model')
+#         with open('models/SentimentAnalysis/tokenizer.pickle', 'rb') as f:
+#             self.tokenizer = pickle.load(f)
+#         self.TEXT_CLEANING_RE = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
 
-    def download_dependencies(self):
-        try:
-            nltk.data.find('stopwords')
-        except LookupError:
-            nltk.download('stopwords')
+#     def download_dependencies(self):
+#         try:
+#             nltk.data.find('stopwords')
+#         except LookupError:
+#             nltk.download('stopwords')
 
-    def process_text(self):
-        self.text = re.sub(self.TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
-        tokens = []
-        stop_words = stopwords.words('english')
-        stemmer = SnowballStemmer('english')
-        for token in text.split():
-            if token not in stop_words:
-                tokens.append(stemmer.stem(token))
-        self.text = " ".join(tokens)
-        self.data = pad_sequences(self.tokenizer.texts_to_sequences(self.text), maxlen=700)
+#     def process_text(self):
+#         self.text = re.sub(self.TEXT_CLEANING_RE, ' ', str(text).lower()).strip()
+#         tokens = []
+#         stop_words = stopwords.words('english')
+#         stemmer = SnowballStemmer('english')
+#         for token in text.split():
+#             if token not in stop_words:
+#                 tokens.append(stemmer.stem(token))
+#         self.text = " ".join(tokens)
+#         self.data = pad_sequences(self.tokenizer.texts_to_sequences(self.text), maxlen=700)
 
-    def predict(self):
-        if self.data.any():
-            prediction = self.model.predict_classes(self.data)
-            if prediction[0] == 0:
-                return "partially false"
-            elif prediction[0] == 1:
-                return "false"
-            elif prediction[0] == 2:
-                return "true"
-            elif prediction[0] == 3:
-                return "other"
+#     def predict(self):
+#         if self.data.any():
+#             prediction = self.model.predict_classes(self.data)
+#             if prediction[0] == 0:
+#                 return "partially false"
+#             elif prediction[0] == 1:
+#                 return "false"
+#             elif prediction[0] == 2:
+#                 return "true"
+#             elif prediction[0] == 3:
+#                 return "other"
 
 
 class Three_Layer(Model):
@@ -484,11 +484,11 @@ if __name__ == '__main__':
     bilstm.process_text()
     prediction_bilstm = bilstm.predict()
 
-    # Sentiment Analysis
-    sentiment = Sentiment(text)
-    sentiment.download_dependencies()
-    sentiment.process_text()
-    prediction_sentiment = sentiment.predict()
+    # # Sentiment Analysis
+    # sentiment = Sentiment(text)
+    # sentiment.download_dependencies()
+    # sentiment.process_text()
+    # prediction_sentiment = sentiment.predict()
 
     # Three-Layer
     three_layer = Three_Layer(text)
